ccf8bde812a14244cd4d2db7896d116697a3428a
Thor Arisland <tcarisland@gmail.com>
2022-11-10 21:59:39 +0100

    fixed broken scripts

--- paths/addcurvepoints.py
+++ paths/addcurvepoints.py
@@ -5,7 +5,7 @@ from Foundation import NSPoint
 def lerp(t, a, b):
 	return NSPoint(int((1-t)*a.x + t*b.x), int((1-t)*a.y + t*b.y))
 
-for p in Glyphs.font.selectedLayers[0].selection[0].paths:
+for p in Glyphs.font.selectedLayers[0].paths:
 	for idx in range(len(p.nodes) -1, 0, -1):
 		node = p.nodes[idx]
 		if node.type == LINE:
